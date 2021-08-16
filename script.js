1022ef3439b7c3e94afd5ccfeb8aa6e6738962bd
Logan Woolf <loganwoolf@gmail.com>
2021-08-15 21:15:44 -0600

    adds functionality to number and backspace

--- script.js
+++ script.js
@@ -29,3 +29,23 @@ function operate(operator, a, b) {
          break
       }
    }
+
+let inputString = "";
+let outputString = "";
+
+const inputField = document.querySelector('.display__input')
+const numberButtons = document.querySelectorAll('.buttons__numbers button')
+const dropButton = document.querySelector('#back')
+
+function handleInput(e) {
+   inputString += e.target.textContent
+   inputField.textContent = inputString;
+}
+function dropDigit() {
+   inputString = inputString.slice(0,inputString.length-1)
+   inputField.textContent = inputString
+}
+
+numberButtons.forEach(button => button.addEventListener('click', handleInput))
+dropButton.addEventListener("click", dropDigit)
+
