c549a8b55408e815445c636b38a4d72a67e6c0d3
Logan Woolf <loganwoolf@gmail.com>
2021-08-15 22:52:47 -0600

    add operation and control button functionality

--- script.js
+++ script.js
@@ -29,23 +29,55 @@ function operate(operator, a, b) {
          break
       }
    }
-
-let inputString = "";
-let outputString = "";
+let inputString = ''
+let currentInput = ''
+let previousInput = ''
+let outputString = ''
+let currentOperation = ''
 
 const inputField = document.querySelector('.display__input')
+const outputField = document.querySelector('.display__output')
 const numberButtons = document.querySelectorAll('.buttons__numbers button')
+const operatorButtons = document.querySelectorAll('.operator')
 const dropButton = document.querySelector('#back')
+const clearButton = document.querySelector('#clear')
+const equalsButton = document.querySelector('#equals')
 
 function handleInput(e) {
-   inputString += e.target.textContent
-   inputField.textContent = inputString;
+   currentInput += e.target.textContent
+   outputField.textContent = currentInput
 }
 function dropDigit() {
-   inputString = inputString.slice(0,inputString.length-1)
-   inputField.textContent = inputString
+   currentInput = currentInput.slice(0,currentInput.length-1)
+   outputField.textContent = currentInput
+}
+function clearScreen() {
+   inputString = ''
+   currentInput = ''
+   previousInput = ''
+   outputString = ''
+   inputField.textContent = currentInput
+   outputField.textContent = outputString
+}
+function handleOperator(e) {
+   currentOperation = e.target.textContent
+   previousInput = currentInput
+   currentInput = ''
+   inputField.textContent = previousInput + ' ' + currentOperation
+}
+function handleEquals() {
+   inputField.textContent += ' ' + currentInput 
+   currentInput = operate(currentOperation, previousInput, currentInput)
+   outputField.textContent = currentInput
 }
 
 numberButtons.forEach(button => button.addEventListener('click', handleInput))
+operatorButtons.forEach(button => button.addEventListener('click', handleOperator))
 dropButton.addEventListener("click", dropDigit)
+clearButton.addEventListener('click', clearScreen)
+equalsButton.addEventListener('click', handleEquals)
 
+/* Not Working Yet */
+// Chaining operations without pressing equals
+// clear stack for new number when pressing number key after pressing equals
+//
